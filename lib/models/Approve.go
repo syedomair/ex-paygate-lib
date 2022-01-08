36d593f135d582d4694953e897c76c0b76d6a7e1
syedomair <syedkhalidomair@gmail.com>
2022-01-08 19:29:35 +0500

    added mockserver

--- lib/models/Approve.go
+++ lib/models/Approve.go
@@ -2,14 +2,16 @@ package models
 
 // Approve Public
 type Approve struct {
-	ID         int    `json:"id" gorm:"column:id"`
-	MerchantID int    `json:"merchant_id" gorm:"column:merchant_id"`
-	CCNumber   string `json:"cc_number" gorm:"column:cc_number"`
-	CCExpiry   string `json:"cc_expiry" gorm:"column:cc_expiry"`
-	Currency   string `json:"currency" gorm:"column:currency"`
-	Amount     string `json:"amount" gorm:"column:amount"`
-	ApproveKey string `json:"approve_key" gorm:"column:approve_key"`
-	CreatedAt  string `json:"created_at" gorm:"column:created_at"`
+	ID            int    `json:"id" gorm:"column:id"`
+	MerchantID    int    `json:"merchant_id" gorm:"column:merchant_id"`
+	CCNumber      string `json:"cc_number" gorm:"column:cc_number"`
+	CCExpiry      string `json:"cc_expiry" gorm:"column:cc_expiry"`
+	Currency      string `json:"currency" gorm:"column:currency"`
+	Amount        string `json:"amount" gorm:"column:amount"`
+	ApproveKey    string `json:"approve_key" gorm:"column:approve_key"`
+	AmountBalance string `json:"amount_balance" gorm:"column:amount_balance"`
+	Status        int    `json:"status" gorm:"column:status"`
+	CreatedAt     string `json:"created_at" gorm:"column:created_at"`
 }
 
 // TableName Public
---
+++ lib/tools/mockserver/mockserver.go
@@ -0,0 +1,15 @@
+package mockserver
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+)
+
+func MockTestServer(method string, url string, jsonInput []byte) (*httptest.ResponseRecorder, *http.Request) {
+
+	var jsonStr = []byte(jsonInput)
+	req, _ := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
+	response := httptest.NewRecorder()
+	return response, req
+}
